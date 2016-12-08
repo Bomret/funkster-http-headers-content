d29def400c92fcd9535ee444a20ac546a92966be
Stefan Reichel <dev@bomret.com>
2016-12-08 23:15:26 +0100

    Added setContent* functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import * as cType from "content-type";
+// tslint:disable-next-line:no-var-requires
 const cDispo = require("content-disposition");
-import { HttpPipe, request } from "funkster-http";
+import { HttpPipe, request, setHeader } from "funkster-http";
+
+export type ContentEncoding =
+  "gzip"
+  | "compress"
+  | "deflate"
+  | "identity"
+  | "br";
 
 export interface ContentHeaders {
-  contentType: cType.MediaType | void;
-  contentLength: number | void;
-  contentLanguage: string | void;
-  contentEncoding: string | void;
-  contentLocation: string | void;
-  contentDisposition: any | void;
+  contentType?: cType.MediaType;
+  contentLength?: number;
+  contentLanguage?: string;
+  contentEncoding?: ContentEncoding;
+  contentLocation?: string;
+  contentDisposition?: any;
 }
 
 function parse<T>(name: string, transform: (value: string) => T, headers: any) {
@@ -16,12 +24,12 @@ function parse<T>(name: string, transform: (value: string) => T, headers: any) {
   return value ? transform(value) : null;
 }
 
-export function parseRequestContentHeaders(handler: (headers: ContentHeaders) => HttpPipe): HttpPipe {
+export function parseContentHeaders(handler: (headers: ContentHeaders) => HttpPipe): HttpPipe {
   return request(req => {
     const contentType = parse<cType.MediaType>("content-type", cType.parse, req.headers);
     const contentLength = parse<number>("content-length", x => parseInt(x, 10), req.headers);
     const contentLanguage = parse<string>("content-language", x => x, req.headers);
-    const contentEncoding = parse<string>("content-encoding", x => x, req.headers);
+    const contentEncoding = parse<ContentEncoding>("content-encoding", x => <ContentEncoding>x, req.headers);
     const contentLocation = parse<string>("content-location", x => x, req.headers);
     const contentDisposition = parse<any>("content-disposition", cDispo.parse, req.headers);
 
@@ -36,4 +44,36 @@ export function parseRequestContentHeaders(handler: (headers: ContentHeaders) =>
 
     return handler(parsedHeaders);
   });
+}
+
+export function setContentType(type: string | cType.MediaType): HttpPipe {
+  if (typeof type === "string") {
+    return setHeader("Content-Type", type);
+  } else {
+    return setHeader("Content-Type", cType.format(type));
+  }
+}
+
+export function setContentDisposition(filenameOrOptions: string | any, options?: any): HttpPipe {
+  if (typeof filenameOrOptions === "string") {
+    return setHeader("Content-Disposition", cDispo(filenameOrOptions, options));
+  } else {
+    return setHeader("Content-Disposition", cDispo(undefined, filenameOrOptions));
+  }
+}
+
+export function setContentLength(length: number): HttpPipe {
+  return setHeader("Content-Length", String(length));
+}
+
+export function setContentLanguage(language: string): HttpPipe {
+  return setHeader("Content-Language", language);
+}
+
+export function setContentEncoding(encoding: ContentEncoding): HttpPipe {
+  return setHeader("Content-Encoding", encoding);
+}
+
+export function setContentLocation(location: string): HttpPipe {
+  return setHeader("Content-Location", location);
 }
--- test/index-spec.ts
+++ test/index-spec.ts
@@ -1,26 +1,37 @@
+import { compose } from "funkster-core";
 import { asRequestListener, Ok } from "funkster-http";
 import * as http from "http";
 import * as request from "supertest";
 
-import { parseRequestContentHeaders } from "../src";
+import {
+  parseContentHeaders,
+  setContentDisposition,
+  setContentEncoding,
+  setContentLanguage,
+  setContentLength,
+  setContentLocation,
+  setContentType
+} from "../src";
 
 describe("When parsing the Content headers from a request", () => {
   describe("and analyzing the Content-Type", () => {
-    const pipe = parseRequestContentHeaders(headers => Ok(JSON.stringify(headers.contentType)));
+    const pipe = parseContentHeaders(headers => Ok(JSON.stringify(headers.contentType)));
     const server = http.createServer(asRequestListener(pipe));
 
     describe("and none is set", () =>
       it("should parse 'null'", () =>
         request(server)
           .get("/")
-          .expect(200, JSON.stringify(null))));
+          .expect(200)
+          .expect(JSON.stringify(null))));
 
     describe("and 'application/json; charset=utf-8' is set", () =>
       it("should parse the correct information", () =>
         request(server)
           .get("/")
           .type("application/json; charset=utf-8")
-          .expect(200, JSON.stringify({
+          .expect(200)
+          .expect(JSON.stringify({
             parameters: {
               charset: "utf-8"
             },
@@ -29,86 +40,95 @@ describe("When parsing the Content headers from a request", () => {
   });
 
   describe("and analyzing the Content-Length", () => {
-    const pipe = parseRequestContentHeaders(headers => Ok(JSON.stringify(headers.contentLength)));
+    const pipe = parseContentHeaders(headers => Ok(JSON.stringify(headers.contentLength)));
     const server = http.createServer(asRequestListener(pipe));
 
     describe("and none is set", () =>
       it("should parse 'null'", () =>
         request(server)
           .get("/")
-          .expect(200, JSON.stringify(null))));
+          .expect(200)
+          .expect(JSON.stringify(null))));
 
     describe("and '246' is set", () =>
       it("should parse the correct information", () =>
         request(server)
           .get("/")
           .set("Content-Length", "246")
-          .expect(200, JSON.stringify(246))));
+          .expect(200)
+          .expect(JSON.stringify(246))));
   });
 
   describe("and analyzing the Content-Language", () => {
-    const pipe = parseRequestContentHeaders(headers => Ok(JSON.stringify(headers.contentLanguage)));
+    const pipe = parseContentHeaders(headers => Ok(JSON.stringify(headers.contentLanguage)));
     const server = http.createServer(asRequestListener(pipe));
 
     describe("and none is set", () =>
       it("should parse 'null'", () =>
         request(server)
           .get("/")
-          .expect(200, JSON.stringify(null))));
+          .expect(200)
+          .expect(JSON.stringify(null))));
 
     describe("and 'de-DE' is set", () =>
       it("should parse the correct information", () =>
         request(server)
           .get("/")
           .set("Content-Language", "de-DE")
-          .expect(200, JSON.stringify("de-DE"))));
+          .expect(200)
+          .expect(JSON.stringify("de-DE"))));
   });
 
   describe("and analyzing the Content-Encoding", () => {
-    const pipe = parseRequestContentHeaders(headers => Ok(JSON.stringify(headers.contentEncoding)));
+    const pipe = parseContentHeaders(headers => Ok(JSON.stringify(headers.contentEncoding)));
     const server = http.createServer(asRequestListener(pipe));
 
     describe("and none is set", () =>
       it("should parse 'null'", () =>
         request(server)
           .get("/")
-          .expect(200, JSON.stringify(null))));
+          .expect(200)
+          .expect(JSON.stringify(null))));
 
     describe("and 'gzip' is set", () =>
       it("should parse the correct information", () =>
         request(server)
           .get("/")
           .set("Content-Encoding", "gzip")
-          .expect(200, JSON.stringify("gzip"))));
+          .expect(200)
+          .expect(JSON.stringify("gzip"))));
   });
 
   describe("and analyzing the Content-Location", () => {
-    const pipe = parseRequestContentHeaders(headers => Ok(JSON.stringify(headers.contentLocation)));
+    const pipe = parseContentHeaders(headers => Ok(JSON.stringify(headers.contentLocation)));
     const server = http.createServer(asRequestListener(pipe));
 
     describe("and none is set", () =>
       it("should parse 'null'", () =>
         request(server)
           .get("/")
-          .expect(200, JSON.stringify(null))));
+          .expect(200)
+          .expect(JSON.stringify(null))));
 
     describe("and '/some.html' is set", () =>
       it("should parse the correct information", () =>
         request(server)
           .get("/")
           .set("Content-Location", "/some.html")
-          .expect(200, JSON.stringify("/some.html"))));
+          .expect(200)
+          .expect(JSON.stringify("/some.html"))));
   });
 
   describe("and analyzing the Content-Disposition", () => {
-    const pipe = parseRequestContentHeaders(headers => Ok(JSON.stringify(headers.contentDisposition)));
+    const pipe = parseContentHeaders(headers => Ok(JSON.stringify(headers.contentDisposition)));
     const server = http.createServer(asRequestListener(pipe));
 
     describe("and none is set", () => {
       it("should parse 'null'", () =>
         request(server)
           .get("/")
-          .expect(200, JSON.stringify(null)));
+          .expect(200)
+          .expect(JSON.stringify(null)));
     });
 
     describe('and "attachment; filename=some.txt" is set', () => {
@@ -116,12 +136,103 @@ describe("When parsing the Content headers from a request", () => {
         request(server)
           .get("/")
           .set("Content-Disposition", 'attachment; filename="some.txt"')
-          .expect(200, JSON.stringify({
+          .expect(200)
+          .expect(JSON.stringify({
             type: "attachment",
             parameters: {
               filename: "some.txt"
             }
           })));
     });
+  });
+});
+
+describe("When setting the Content headers of a response", () => {
+  describe("and setting the Content-Type to 'application/json' via string", () => {
+    const pipe = compose(setContentType("application/json"), Ok());
+    const server = http.createServer(asRequestListener(pipe));
+
+    it("should set the response header correctly", () =>
+      request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Type", "application/json"));
+  });
+
+  describe("and setting the Content-Type to 'application/json' via a MediaType object", () => {
+    const pipe = compose(setContentType({ type: "application/json", parameters: { charset: "utf-8" } }), Ok());
+    const server = http.createServer(asRequestListener(pipe));
+
+    it("should set the response header correctly", () =>
+      request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Type", "application/json; charset=utf-8"));
+  });
+
+  describe("and setting the Content-Disposition to 'attachment; filename=some.txt' via string", () => {
+    const pipe = compose(setContentDisposition("attachment; filename=some.txt"), Ok());
+    const server = http.createServer(asRequestListener(pipe));
+
+    it("should set the response header correctly", () =>
+      request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Disposition", "attachment; filename=some.txt"));
+  });
+
+  describe("and setting the Content-Type to 'attachment; filename=some.txt' via a ContentDisposition object", () => {
+    const pipe = compose(setContentDisposition("some.txt"), Ok());
+    const server = http.createServer(asRequestListener(pipe));
+
+    it("should set the response header correctly", () =>
+      request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Disposition", 'attachment; filename="some.txt"'));
+  });
+
+  describe("and setting the Content-Length to 246", () => {
+    const pipe = compose(setContentLength(246), Ok());
+    const server = http.createServer(asRequestListener(pipe));
+
+    it("should set the response header correctly", () =>
+      request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Length", "246"));
+  });
+
+  describe("and setting the Content-Language to 'de-DE'", () => {
+    const pipe = compose(setContentLanguage("de-DE"), Ok());
+    const server = http.createServer(asRequestListener(pipe));
+
+    it("should set the response header correctly", () =>
+      request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Language", "de-DE"));
+  });
+
+  describe("and setting the Content-Encoding to 'gzip'", () => {
+    const pipe = compose(setContentEncoding("gzip"), Ok());
+    const server = http.createServer(asRequestListener(pipe));
+
+    it("should set the response header correctly", () =>
+      request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Encoding", "gzip"));
+  });
+
+  describe("and setting the Content-Location to '/some.html'", () => {
+    const pipe = compose(setContentLocation("/some.html"), Ok());
+    const server = http.createServer(asRequestListener(pipe));
+
+    it("should set the response header correctly", () =>
+      request(server)
+        .get("/")
+        .expect(200)
+        .expect("Content-Location", "/some.html"));
   });
 });
